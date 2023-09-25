e5d4ff21f5f8808b5be1dda8b25e8bea4988c66b
Ederson-RL <edersonramirezlopez@gmail.com>
2023-09-25 05:34:02 -0500

    Funcionando registro e inicio de sesion

--- src/main/java/com/hyg/proyecto/controller/controller.java
+++ src/main/java/com/hyg/proyecto/controller/controller.java
@@ -10,14 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
 import com.hyg.proyecto.interfazService.IproductoService;
 import com.hyg.proyecto.model.Producto;
 
-
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-
 
 @Controller
 @RequestMapping
@@ -25,98 +22,109 @@ public class controller {
 
     @Autowired
     private IproductoService service;
-    
+
     @GetMapping("/listar")
-    public String listar(Model model){
-        List<Producto>productos=service.listar();
+    public String listar(Model model) {
+        List<Producto> productos = service.listar();
         model.addAttribute("productos", productos);
         return "indexP";
 
     }
-        @GetMapping("/new")
-    public String agregar(Model model){
-        
+
+    @GetMapping("/new")
+    public String agregar(Model model) {
+
         model.addAttribute("producto", new Producto());
         return "form";
 
     }
+
     @PostMapping("/save")
-    public String save(@Validated Producto p, Model model){
-        
+    public String save(@Validated Producto p, Model model) {
+
         service.save(p);
         return "redirect:/listar";
 
     }
+
     @GetMapping("/editar/{id}")
-    public String editar(@PathVariable int id, Model model){
-         Optional<Producto>Producto=service.listarId(id);
-        model.addAttribute("producto",  Producto);
+    public String editar(@PathVariable int id, Model model) {
+        Optional<Producto> Producto = service.listarId(id);
+        model.addAttribute("producto", Producto);
         return "form";
-}
-   @GetMapping("/eliminar/{id}")
-    public String delete( Model model,@PathVariable int id){
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String delete(Model model, @PathVariable int id) {
         service.delete(id);
         return "redirect:/listar";
 
     }
-    @GetMapping("/login")
-    public String visualzarlogin(Model modelo){
-   
-               return "login";
-       }
-    
-    
-   @GetMapping("/registrar")
-    public String visualzarregistra(Model modelo){
-   
-               return "registrar";
-       }
 
-   @GetMapping("/index")
-    public String visualzarindex(Model modelo){
-   
-               return "inicio";
-       }
-         
+    @GetMapping("/login")
+    public String visualzarlogin(Model modelo) {
+
+        return "loginh";
+    }
+
+    @PostMapping("/login")
+    public String visualzarloginp(Model modelo) {
+
+        return "loginh";
+    }
+
+    @GetMapping("/registrar")
+    public String visualzarregistra(Model modelo) {
+
+        return "registrar";
+    }
+
+    @GetMapping("/index")
+    public String visualzarindex(Model modelo) {
+
+        return "inicio";
+    }
+
     @GetMapping("/catalogo")
-    public String visualzarCatalogo(Model modelo){
-   
-               return "catalogo";
-       }
-    
+    public String visualzarCatalogo(Model modelo) {
+
+        return "catalogo";
+    }
 
     @GetMapping("/Nosotros")
-    public String visualzarnosotros(Model modelo){
-   
-               return "Nosotros";
-       }
+    public String visualzarnosotros(Model modelo) {
+
+        return "Nosotros";
+    }
 
     @GetMapping("/contacto")
-    public String visualzarcoontacto(Model modelo){
-   
-               return "Contacto";
-       }
+    public String visualzarcoontacto(Model modelo) {
+
+        return "Contacto";
+    }
 
     @GetMapping("/dashboardAdmin")
-    public String visualzardashboardAdmin(Model modelo){
-   
-               return "DashboardAdmin";
-       }
+    public String visualzardashboardAdmin(Model modelo) {
+
+        return "DashboardAdmin";
+    }
+
     @GetMapping("/dashboardCliente")
-    public String visualzardashboardCliente(Model modelo){
-   
-               return "DashboardCliente";
-       }
+    public String visualzardashboardCliente(Model modelo) {
+
+        return "DashboardCliente";
+    }
+
     @GetMapping("/Error404")
-    public String visualzarError404(Model modelo){
-   
-               return "Error404";
-       }
+    public String visualzarError404(Model modelo) {
+
+        return "Error404";
+    }
+
     @GetMapping("/Error500")
-    public String visualzarError500(Model modelo){
-   
-               return "Error500";
-       }
-    
+    public String visualzarError500(Model modelo) {
+
+        return "Error500";
+    }
 
 }
